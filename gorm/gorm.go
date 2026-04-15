16c0c692c06eb284303d37de7175d1fdd3d50861
Diego Alba <d.alba.work@gmail.com>
2026-04-14 21:55:20 -0400

    feat: gorm bridge works

--- gorm/gorm.go
+++ gorm/gorm.go
@@ -2,10 +2,18 @@ package gorm
 
 import (
 	"errors"
+	"log"
+	"log/slog"
 	"net/http"
 	"reflect"
 
 	"gorm.io/gorm"
+)
+
+var (
+	logger = slog.New(slog.NewJSONHandler(log.Writer(), &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
 )
 
 type GormOptions struct {
@@ -26,6 +34,20 @@ type _gorm struct {
 	Options GormOptions
 }
 
+func (g *_gorm) SetModifiers(modifiers map[string]string) _gorm {
+	newg := *g
+	newg.Options.KeyModifiers = modifiers
+
+	return newg
+}
+
+func (g *_gorm) SetResult(result any) _gorm {
+	newg := *g
+	newg.Options.Result = result
+
+	return newg
+}
+
 func (g _gorm) Data(w http.ResponseWriter, r *http.Request, m map[string]any) (any, error) {
 	if g.db == nil {
 		return nil, errors.New("gorm: database connection is nil")
@@ -42,6 +64,7 @@ func (g _gorm) Data(w http.ResponseWriter, r *http.Request, m map[string]any) (a
 			if modifier, ok := g.Options.KeyModifiers[k]; ok {
 				equality = modifier
 			}
+			logger.Debug("gorm: adding query condition", "key", k, "value", value, "modifier", equality)
 			tx.Where(k+equality+"?", value)
 		}
 	}
@@ -64,10 +87,15 @@ func (g _gorm) Data(w http.ResponseWriter, r *http.Request, m map[string]any) (a
 	}
 
 	slicePtr := reflect.New(reflect.SliceOf(t))
-	if err := tx.Find(slicePtr.Interface()).Error; err != nil {
-		return nil, err
+	tx = tx.Find(slicePtr.Interface())
+	if tx.Error != nil {
+		logger.Error("gorm: failed to execute query", "error", tx.Error)
+		return nil, tx.Error
+	} else if tx.RowsAffected == 0 {
+		return nil, errors.New("gorm: no records found")
 	}
 
+	logger.Info("gorm: query executed successfully")
 	return slicePtr.Interface(), nil
 }
 
@@ -80,6 +108,7 @@ func (g _gorm) Name() string {
 func GORM(name string, model any, result any, db *gorm.DB, options GormOptions) _gorm {
 	return _gorm{
 		name: name,
+		db:   db,
 
 		Options: GormOptions{
 			Model:        model,
